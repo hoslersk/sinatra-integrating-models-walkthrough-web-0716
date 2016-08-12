b7756d25b31cc1e5be53bf42c2512bb9c2433262
Schyler Hosler <schylerhosler@gmail.com>
2016-08-11 20:30:13 -0400

    Initial commit:

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ end
 
 post '/' do
   text_from_user = params[:user_text]
+  @analyzed_text = TextAnalyzer.new(text_from_user)
+  #or simply...
+  #@analyzed_text = TextAnalyzer.new(params[:user_text])
   erb :results
 end
 
